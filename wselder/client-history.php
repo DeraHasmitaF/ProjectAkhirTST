4a10648621eaa56465af8c42e081008ae3c027a5
DeraHasmitaF <148057648+DeraHasmitaF@users.noreply.github.com>
2024-12-11 18:07:07 +0700

    Delete wselder directory

--- wselder/client-history.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$wsdl = "http://localhost/wselder/VoteAsses.wsdl";
-$client = new SoapClient($wsdl);
-$histories = $client->getHistories(); 
-
-    header('Content-Type: application/json');
-    echo json_encode($histories);
--- wselder/client-vote.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$wsdl = "http://localhost/wselder/VoteAsses.wsdl";
-$client = new SoapClient($wsdl);
-    $tugasLMS = true; 
-    $vote = $client->getVote($tugasLMS);
-
-    header('Content-Type: application/json');
-    echo json_encode($vote);
